f41cac285fe2fc2680d8aa3e5eeac62df50b107d
Md.Rabiul Hasan <70057377+md-rabiul-hasan@users.noreply.github.com>
2022-11-09 12:12:54 +0600

    imperative handle hooks

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import ReducerTutorial from './hooks/useReducer/ReducerTutorial';
 import EffectTutorial from './hooks/useEffect/EffectTutorial';
 import RefTutorial from './hooks/useRef/RefTutorial';
 import LayoutTutorial from './hooks/useLayout/LayoutTutorial';
+import ImperativeHandle from './hooks/useImperativeHandle/ImperativeHandle';
 
 function App() {
   return (
     <div className="App">
-        <LayoutTutorial />
+        <ImperativeHandle />
     </div>
   );
 }
